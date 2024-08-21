3af7ab6e9ce3efd3108866944892881fe0cc6077
PremBhooma <bhoomasagar1213@gmail.com>
2024-08-21 09:31:00 +0530

    code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import App from "./App";
 import React from "react";
 import Context from "./Pages/Context";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
